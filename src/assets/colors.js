05a3b54d24e17b32c49fbdd5c68608b5d89edad1
Felirian <hhghv49@gmail.com>
2023-09-06 11:56:34 +0300

    add: wrapper effect

---
+++ src/assets/colors.js
@@ -0,0 +1,7 @@
+import p1 from './colors/v1_001.jpg'
+import p2 from './colors/v1_021.jpg'
+import p3 from './colors/v1_031.jpg'
+import p4 from './colors/v1_041.jpg'
+import p5 from './colors/v1_051.jpg'
+
+export const colors = [p1, p2, p3, p4, p5]
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,77 +2,63 @@ import React, {useEffect} from 'react';
 import s from '../styles/pages/main.module.scss'
 import {gsap} from "gsap";
 import {ScrollTrigger} from "gsap/ScrollTrigger";
-
-import img_1 from '../assets/img_1.jpg'
+import {colors} from "../assets/colors";
 
 const Main = () => {
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger)
+    // const wrapperTrigger = ScrollTrigger.create ({
+    //   trigger : 'button',
+    //   start: 'top 30%',
+    //   end: 'center 90%',
+    //   scrub: true,
+    //   markers: true
+    // })
 
-    gsap.to('#logo', {
+    gsap.from('.wrapper', {
       scrollTrigger: {
-        trigger: '#logo',
-        start: "center 40%",
-        end:"bottom 60%",
-        markers:true,
-        scrub: true
+        trigger : '.wrapper',
+        start: 'top 70%',
+        end: '10% 70%',
+        scrub: 1,
+        markers: 1
       },
-      height: 60,
-      ease: true
+      width: '80%',
+
     })
-    gsap.to('#logo_title', {
-      scrollTrigger: {
-        trigger: '#logo',
-        start: "center 40%",
-        end:"bottom 60%",
-        markers:true,
-        scrub: true
-      },
-      fontSize: "50px",
-      fontWeight: 300,
-      ease: true
+    const scrollPosition = () => window.pageYOffset;
+
+    const canvas = document.getElementsByTagName('canvas')[0]
+    const ctx = canvas.getContext("2d");
+    const image = new Image(1920, 1080)
+    image.onload = drawImageActualSize;
+    document.addEventListener('scroll', e => {
+      console.log(colors)
+      image.src = colors[Math.round(scrollPosition() / 100)]
     })
-    // gsap.to("#image_wrapper", {
-    //   scrollTrigger: {
-    //     trigger: "#image_wrapper",
-    //     scrub: true,
-    //     pin: true,
-    //     start: "center center",
-    //     end: "bottom -100%",
-    //     toggleClass: "active",
-    //     ease: "power2"
-    //   }
-    // });
-    //
-    // gsap.to("#image", {
-    //   scrollTrigger: {
-    //     trigger: "#image_wrapper",
-    //     scrub: 0.5,
-    //     start: "top bottom",
-    //     end: "bottom -300%",
-    //     ease: "power2"
-    //   },
-    //   y: "-30%"
-    // });
+
+    function drawImageActualSize() {
+      console.log('load')
+      canvas.width = this.naturalWidth
+      canvas.height = this.naturalHeight;
+      ctx.drawImage(this, 0, 0);
+    }
 
   }, []);
   return (
     <div className={s.main}>
-      <div className={s.logo} id={'logo'}>
-        <h1 className={s.logo_title} id={'logo_title'}>FELiRiAN</h1>
+      <div className={s.null}>
+        <canvas width={'100%'} height={'100vh'} id={'v0'}></canvas>
       </div>
 
-      <div className={s.null} style={{marginTop: '50vh', backgroundColor: "red"}}>
+
+      <div className={s.null} style={{marginTop: '50vh', color: "black"}}>
         <h1>1 Main</h1>
       </div>
 
-      {/*<div className={s.null}>*/}
-      {/*  <div id={'image_wrapper'} className={s.image_wrapper}>*/}
-      {/*    <img id={'image'} src={img_1} alt={'none'}/>*/}
-      {/*    <h1>hoko</h1>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
+      <div className={s.wrapper}>
 
+      </div>
 
       <div className={s.null}>
         <h1>2 Main</h1>
@@ -88,6 +74,7 @@ const Main = () => {
       <div className={s.null}>
         <h1>5 Main</h1>
       </div>
+
 
       <div id={'scrollElement'}/>
     </div>
