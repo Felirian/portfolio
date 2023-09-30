c35213c83e3832d9044fd77d8e43036c4277f27f
Felirian <hhghv49@gmail.com>
2023-10-01 02:53:30 +0300

    add: main page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React from "react";
 import "./styles/styles.scss"
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
-import Main from "./pages/Main";
-import MapPage from "./pages/MapPage";
+import Main from "./pages/Main/Main";
 import TestPage from "./pages/TestPage";
-import {ParallaxProvider} from 'react-scroll-parallax';
-document.addEventListener('DOMContentLoaded', ev => {
-  console.log('ready')
-})
+
 function App() {
   return (
-
     <BrowserRouter>
-
+      <p className={'water_snack'}>@FELIRIAN</p>
       <Routes>
         <Route path={'/*'} element={<Main/>}/>
-        <Route path={'/map'} element={<MapPage/>}/>
         <Route path={'/test'} element={<TestPage/>}/>
         <Route path="*" element={<Navigate to={'/'} replace/>}/>
       </Routes>
 
     </BrowserRouter>
-
-
   );
 }
 
---
+++ src/pages/Main/Main.js
@@ -0,0 +1,89 @@
+import React, {useLayoutEffect} from 'react';
+import s from './main.module.scss'
+import inscriptionIMG from '../../assets/main/inscription.png'
+import {gsap} from 'gsap'
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+import {Link} from "react-router-dom";
+
+const Main = () => {
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger)
+    const TAB_LINE = document.getElementsByClassName(s.line)
+
+    const tabsTrigger = {
+      trigger: '#tabs',
+      start: '0% 100%',
+      end: '90% 100%',
+      scrub: 3,
+      markers: 1
+    }
+
+    for (let i = 0; i < TAB_LINE.length; i++) {
+      gsap.from(TAB_LINE[i], {
+        scrollTrigger: {
+          trigger: '#tabs',
+          start: '10% 100%',
+          end: '90% 100%',
+          scrub: i,
+          markers: 1
+        },
+        ease: "power1.inOut",
+        height: 0,
+        opacity: 0,
+
+        //filter: 'blur(50px)',
+      })
+    }
+
+    const inscriptionTrigger = {
+      trigger: '#inscription',
+      start: '10% 0%',
+      end: '70% 0%',
+      scrub: 3,
+      markers: 0
+    }
+
+    gsap.to('#inscription_text', {
+      scrollTrigger: inscriptionTrigger,
+      ease: "power1.inOut",
+      left: '100vw',
+      opacity: 0,
+      //filter: 'blur(50px)',
+    })
+    gsap.to('#inscription_img', {
+      scrollTrigger: inscriptionTrigger,
+      ease: "power1.inOut",
+      right: '-40%',
+      filter: 'blur(50px)',
+    })
+
+  }, []);
+
+  return (
+    <div className={s.main}>
+      <div className={s.inscription} id={'inscription'}>
+        <h1 id={'inscription_text'}>PORTFOLIO</h1>
+        <img src={inscriptionIMG} alt={'image'} id={'inscription_img'}/>
+      </div>
+      <div className={s.tabs} id={'tabs'}>
+        <Link to={'/test'} className={s.tab}>
+          <h3>coding</h3>
+        </Link>
+        <div className={s.line}/>
+        <Link className={s.tab}>
+          <h3>photo</h3>
+        </Link>
+        <div className={s.line}/>
+        <Link className={s.tab}>
+          <h3>ux / ui</h3>
+        </Link>
+        <div className={s.line}/>
+        <Link className={s.tab}>
+          <h3>modeling</h3>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
