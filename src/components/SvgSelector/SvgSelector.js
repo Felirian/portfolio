d22dacd2f4096dd2801ae525c6412295a17dd2fc
Felirian <141245468+Felirian@users.noreply.github.com>
2024-01-23 22:58:50 +0300

    add: burger anim

--- src/components/SvgSelector/SvgSelector.js
+++ src/components/SvgSelector/SvgSelector.js
@@ -7,16 +7,7 @@ const SvgSelector = ({svg}) => {
     case "Burger":
       return (
         <>
-          <style>
-            {`
-              rect {transition: 0.5s;}
-              svg:hover #first {x: 8}
-              svg:hover #second {x: 0}
-              svg:hover #third {x: 8}
-             `}
-          </style>
           <svg className={s.Burger} viewBox="0 0 24 24" width="40" height="40">
-
             <g id="_01_align_center" data-name="01 align center" fill={'white'}>
               <rect id={'first'} x="4" y="6" width="16" height="1"/>
               <rect id={'second'} x="4" y="11" width="16" height="1"/>
