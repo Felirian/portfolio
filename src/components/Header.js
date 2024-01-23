f3260fb810dba1358ec47219adc4ed997d04e169
Felirian <141245468+Felirian@users.noreply.github.com>
2024-01-23 22:58:50 +0300

    add: burger anim

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import styled from "styled-components";
+import React, {useState} from 'react';
+import styled, {keyframes} from "styled-components";
 import {NavLink} from "react-router-dom";
 import SvgSelector from "./SvgSelector/SvgSelector";
+import {COLORS} from "../shared/variables";
 
 const Header = () => {
+  const [open, setOpen] = useState(false)
+
+
   return (
     <HeaderWrapper>
-      <SvgSelector svg={'Burger'}/>
+
+      <Burger onClick={() => setOpen(!open)} isOpen={open}>
+        <SvgSelector svg={'Burger'}/>
+
+      </Burger>
+
     </HeaderWrapper>
   );
+
 };
 
 const HeaderWrapper = styled.header`
-    width: 100vh;
-    height: 100vh;
-    display: block;
-    position: fixed;
-    top: 0;
-    right: 0;
-    
+  width: 100vw;
+  height: 100vh;
+  display: block;
+  position: fixed;
+  top: 0;
+  right: 0;
+`
+const Burger = styled.div`
+  right: 0;
+  position: absolute;
+
+  rect {transition: 0.5s;}
+  svg:hover #first {x: 6}
+  svg:hover #second {x: 2}
+  svg:hover #third {x: 6}
+
+  #first {
+    transform: 
+      rotate(${(props) => (props.isOpen ? '45deg' : '0deg')}) 
+      translateX(${(props) => (props.isOpen ? '5px' : '0px')}) 
+      translatey(${(props) => (props.isOpen ? '-2px' : '0px')});
+    transition: 0.5s;
+  }
+
+  #second {
+    width: ${(props) => (props.isOpen ? '0' : '16')};
+    transform: translateX(${(props) => (props.isOpen ? '12px' : '0px')});
+    transition: 0.5s;
+  }
+
+  #third {
+    transform: 
+      rotate(${(props) => (props.isOpen ? '-45deg' : '0deg')}) 
+      translateX(${(props) => (props.isOpen ? '-17px' : '0px')}) 
+      translatey(${(props) => (props.isOpen ? '0px' : '0px')});
+    transition: 0.5s;
+  }
 `
 
 export default Header;
