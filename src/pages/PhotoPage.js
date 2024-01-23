29a9ee57b0e0c9b65a711bd07be9e284aa6302b0
Felirian <141245468+Felirian@users.noreply.github.com>
2024-01-24 01:12:52 +0300

    add: img paralax

--- src/pages/PhotoPage.js
+++ src/pages/PhotoPage.js
@@ -1,11 +1,53 @@
 import React from 'react';
+import styled from "styled-components";
+import ImagePrlx from "../components/ImagePrlx";
+
+import img11 from '../assets/Photos/1.1.jpg'
 
 const PhotoPage = () => {
   return (
-    <div>
+    <PhotoPageWrapper>
+      <Null/>
       <h1>photo</h1>
-    </div>
+      <ImagePrlx
+        img={img11}
+        height={66.17}
+      />
+      <ImagePrlx
+        img={img11}
+        height={66.17}
+      />
+      <ImagePrlx
+        img={img11}
+        height={66.17}
+      />
+      <Null/>
+      <Null/>
+    </PhotoPageWrapper>
   );
 };
+
+const Null = styled.div`
+  width: 100vw;
+  height: 100vh;
+`
+
+const PhotoPageWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 0px 10px;
+  flex-direction: column;
+  align-items: center;
+  gap: 400px;
+`
+
+const Block = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 3.91vw;
+  align-self: stretch;
+`
 
 export default PhotoPage;
